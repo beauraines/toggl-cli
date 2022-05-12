dc0d7cb67678e2b13aab5bfcd93df027c933cec4
Beau Raines <beau.raines@gmail.com>
2022-05-11 20:30:05 -0700

    client gets token from config

--- client.js
+++ client.js
@@ -1,30 +1,16 @@
 const togglClient = require('toggl-client');
 require('dotenv').config();
+const config = require('./config'); 
 
 module.exports = function() {
-
-    if (!process.env.TOGGL_API_TOKEN) {
-        console.log('TOGGL_API_TOKEN environment variable is not set.');
-        console.log('For development, it can be set in the .env file in the project root');
+    const apiToken = config.apiToken;
+    let client;
+    try {
+        client = togglClient({ apiToken });
+    } catch (error) {
+       console.error(error); 
     }
     
-    // TODO Try to read rc file
-    
-    // FIXME apiToken is not needed
-    const apiToken = process.env.TOGGL_API_TOKEN;
-        const client = togglClient();
-        // const client = togglClient({ apiToken });
-    // ? Why doesn't a try/catch block work?
-    // try {
-    //     const client = togglClient({ apiToken });
-    // } catch (error) {
-    //    console.error(error); 
-    // }
-    if (!client) {
-        console.error('There was a problem');
-        process.exit(1);
-    }
-
     return client;
     
 }
