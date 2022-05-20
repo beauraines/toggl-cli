3476fd69e59da61e677e906ee3630b3ab5ac92be
Beau Raines <beau.raines@gmail.com>
2022-05-19 21:48:32 -0700

    adds formatting functions

--- cmds/ls.js
+++ cmds/ls.js
@@ -1,4 +1,5 @@
 const Client = require('../client');
+const dayjs = require('dayjs');
 
 
 exports.command = 'ls'
@@ -9,8 +10,37 @@ exports.builder = {
 exports.handler = async function (argv) {
     let client = Client();
     timeEntries = await client.timeEntries.list();
-    console.table(timeEntries,['description','start','stop','duration'])
+
+    let report =[];
+    timeEntries.forEach(element => {
+        report.push(
+            {
+                description: element.description,
+                start: formatDateTime(element.start),
+                stop: formatDateTime(element.stop),
+                duration: formatDuration(element.duration)
+            }
+        )
+    });
+
+    console.table(report,['description','start','stop','duration'])
 }
 
-
-
+// TODO move these to a utility module
+/**
+ * Formats a dateTime to YYYY-MM-DD HH:mm
+ * @param {Date} dateTime 
+ * @returns {String}
+ */
+function formatDateTime(dateTime) {
+    let tz = process.env.TOGGL_TIMEZONE || 'America/New_York';
+    return dayjs(dateTime).tz(tz).format('YYYY-MM-DD HH:mm');
+}
+/**
+ * Converts duration in milliseconds to hours minutes
+ * @param {Number} duration 
+ * @returns {String} 
+ */
+function formatDuration(duration) {
+    return dayjs.duration(duration*1000).format('H[h] m[m]');
+}
