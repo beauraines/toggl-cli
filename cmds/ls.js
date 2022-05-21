b9701f6c7ee58f6f3ee10512ca52a7fef8bb52fe
Beau Raines <beau.raines@gmail.com>
2022-05-21 09:18:55 -0700

    formats ls command output

--- cmds/ls.js
+++ cmds/ls.js
@@ -1,6 +1,6 @@
 const Client = require('../client');
 const dayjs = require('dayjs');
-
+const utils = require('../utils');
 
 exports.command = 'ls'
 exports.desc = 'Lists time entries'
@@ -16,9 +16,9 @@ exports.handler = async function (argv) {
         report.push(
             {
                 description: element.description,
-                start: formatDateTime(element.start),
-                stop: formatDateTime(element.stop),
-                duration: formatDuration(element.duration)
+                start: utils.convertUtcTime(element.start),
+                stop: utils.convertUtcTime(element.stop),
+                duration: utils.formatDuration(element.duration*1000)
             }
         )
     });
@@ -26,21 +26,4 @@ exports.handler = async function (argv) {
     console.table(report,['description','start','stop','duration'])
 }
 
-// TODO move these to a utility module
-/**
- * Formats a dateTime to YYYY-MM-DD HH:mm
- * @param {Date} dateTime 
- * @returns {String}
- */
-function formatDateTime(dateTime) {
-    let tz = process.env.TOGGL_TIMEZONE || 'America/New_York';
-    return dayjs(dateTime).tz(tz).format('YYYY-MM-DD HH:mm');
-}
-/**
- * Converts duration in milliseconds to hours minutes
- * @param {Number} duration 
- * @returns {String} 
- */
-function formatDuration(duration) {
-    return dayjs.duration(duration*1000).format('H[h] m[m]');
-}
+
