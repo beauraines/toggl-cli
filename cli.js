17bb9042e7913996a1b04e90b2dbad0e53cae137
Beau Raines <beau.raines@gmail.com>
2022-05-26 17:04:23 -0700

    Adds command line completion
    Resolves #6

--- cli.js
+++ cli.js
@@ -6,8 +6,9 @@ const yargs = require("yargs");
 
 
 require('yargs/yargs')(process.argv.slice(2))
-  .scriptName('toggl')
+  .scriptName('toggl-node')
   .commandDir('cmds')
+  .completion('completion','Outputs bash/zsh-completion shortcuts for commands and options to add to .bashrc or .bash_profile')
   .demandCommand()
   .help()
   .argv
