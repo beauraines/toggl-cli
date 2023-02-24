1a0f7400ad1f5a7bfef5ab10a90b77b0b035b2e3
Beau Raines <beau.raines@gmail.com>
2023-02-24 06:49:09 -0800

    Updates start time entry standalone command to use yargs ESM6

--- standalone/startTimeEntry.js
+++ standalone/startTimeEntry.js
@@ -1,14 +1,15 @@
 import togglClient from 'toggl-client'
 import dayjs from 'dayjs'
 import yargs from 'yargs'
+import { hideBin } from 'yargs/helpers'
 import {defaultWorkspaceId,defaultProjectId} from '../utils.js'
 import dotenv from 'dotenv'
 dotenv.config()
 
 const client = togglClient()
 
-// TODO update yargs for ESM
-const options = yargs
+
+const options = yargs(hideBin(process.argv))
   .usage('Usage: $0 -p <project_id> -w <workspace_id> [time entry description]')
   .option('p', { alias: 'projectId', describe: 'project id', type: 'string', demandOption: false })
   .option('w', { alias: 'workspaceId', describe: 'workspace id', type: 'number', demandOption: false })
@@ -20,15 +21,15 @@ async function main () {
   const description = options._.join(' ')
   // TODO check that description was provided or provide a default
   const params = { description }
-  params.workspaceId = defaultWorkspaceId
-  params.projectId = defaultProjectId
+  params.workspaceId = options.workspaceId || defaultWorkspaceId
+  params.projectId = options.projectId || defaultProjectId
   console.debug(params)
   const timeEntry = await createTimeEntry(params)
-  console.info(`Started ${timeEntry.description}`)
+  console.info(`Started ${timeEntry.description} with project id ${timeEntry.pid}`)
 }
 
 async function createTimeEntry (params) {
-  const client = Client()
+//   const client = Client()
   const timeEntry = await client.timeEntries.create(
     {
       description: params.description,
@@ -40,6 +41,7 @@ async function createTimeEntry (params) {
       at: dayjs().toISOString()
     }
   )
+//   console.log(timeEntry)
   return timeEntry
 }
 
