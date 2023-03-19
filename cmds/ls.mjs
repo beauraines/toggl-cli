55c00d3b963e55a41b14495b5468c17805d2515f
Beau Raines <beau.raines@gmail.com>
2023-03-18 18:31:35 -0700

    chore: lintfix

--- cmds/ls.mjs
+++ cmds/ls.mjs
@@ -4,7 +4,7 @@ import dayjs from 'dayjs'
 import debugClient from 'debug'
 import Table from 'cli-table3'
 
-const debug = debugClient('toggl-cli-ls');
+const debug = debugClient('toggl-cli-ls')
 
 export const command = 'ls [searchStrings...]'
 export const desc = 'Lists recent time entries. Defaults to the last 14 days.'
@@ -18,14 +18,15 @@ export const handler = async function (argv) {
   const client = Client()
   const days = argv.days
   let timeEntries = await client.timeEntries.list({
-	  start_date: dayjs().subtract(days, 'days').startOf('day').toISOString(),
-	  end_date: dayjs().toISOString()
+    start_date: dayjs().subtract(days, 'days').startOf('day').toISOString(),
+    end_date: dayjs().toISOString()
   })
   timeEntries.sort((a, b) => (a.start > b.start) ? 1 : -1)
-  if ( argv.searchStrings ) {
-	  const searchString = argv.searchStrings.join(' ')
-	  debug(searchString)
-	  timeEntries = timeEntries.filter(x => x.description.includes(searchString))}
+  if (argv.searchStrings) {
+    const searchString = argv.searchStrings.join(' ')
+    debug(searchString)
+    timeEntries = timeEntries.filter(x => x.description.includes(searchString))
+  }
   const report = []
   timeEntries.forEach(element => {
     report.push(
