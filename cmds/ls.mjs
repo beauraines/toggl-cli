39df0223f2da2eb958f317e02c7652dd114dfc55
Beau Raines <beau.raines@gmail.com>
2023-03-18 18:07:45 -0700

    chore: adds debug to ls command

--- cmds/ls.mjs
+++ cmds/ls.mjs
@@ -1,7 +1,10 @@
 import Client from '../client.js'
 import { convertUtcTime, formatDuration } from '../utils.js'
 import dayjs from 'dayjs'
+import debugClient from 'debug'
 import Table from 'cli-table3'
+
+const debug = debugClient('toggl-cli-ls');
 
 export const command = 'ls'
 export const desc = 'Lists recent time entries. Defaults to the last 14 days.'
@@ -11,6 +14,7 @@ export const builder = {
 }
 
 export const handler = async function (argv) {
+  debug(argv)
   const client = Client()
   const days = argv.days
   const timeEntries = await client.timeEntries.list({
