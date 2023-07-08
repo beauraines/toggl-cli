9ebdf8e40bd6ef74ad9b0b2cb7f1073339029b08
Beau Raines <beau.raines@gmail.com>
2023-07-08 13:45:39 -0700

    adds create-config command

---
+++ cmds/create-config.mjs
@@ -0,0 +1,28 @@
+import { config } from "@beauraines/node-helpers";
+import debugClient from 'debug';
+
+const debug = debugClient('toggl-cli-create-config');
+
+export const command = 'create-config'
+export const desc = 'Creates a boilerplate .toggl-cli.json configuration file in your home directory.'
+export const builder = {}
+
+export const handler = async function (argv) {
+    const configProps = [ 
+        "api_token",
+        "default_workspace_id",
+        "timezone",
+        "default_project_id"
+    ]
+
+    let configFile
+    try {
+        configFile = await config.createConfig('.toggl-cli.json',configProps)
+        console.log(`Configuration file written to ${configFile} in your home directory`)
+        console.log(`Edit with your user information from Toggl.`)
+    } catch (error) {
+        console.error(error.message)
+        process.exit(1)
+    }
+
+}
--- cmds/index.mjs
+++ cmds/index.mjs
@@ -10,6 +10,7 @@ import * as startTimeEntry from './startTimeEntry.mjs'
 import * as stopTimeEntry from './stopTimeEntry.mjs'
 import * as today from './today.mjs'
 import * as weekly from './weekly.mjs'
+import * as createConfig from './create-config.mjs'
 export const commands = [
   continueEntry,
   current,
@@ -22,5 +23,6 @@ export const commands = [
   today,
   web,
   weekly,
-  workspace
+  workspace,
+  createConfig
 ]
